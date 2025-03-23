476c20a4b599fda09e70ea80e41cf4240c08537c
SolarFactories <SolarFactories@users.noreply.github.com>
2025-03-23 23:38:01 +0000

    Added swid attribute to dependencytrack_project.

--- internal/provider/project_resource_test.go
+++ internal/provider/project_resource_test.go
@@ -24,6 +24,7 @@ resource "dependencytrack_project" "test" {
 					resource.TestCheckResourceAttr("dependencytrack_project.test", "description", ""),
 					resource.TestCheckResourceAttr("dependencytrack_project.test", "version", ""),
 					resource.TestCheckNoResourceAttr("dependencytrack_project.test", "parent"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "classifier", "APPLICATION"),
 				),
 			},
 			// ImportState testing
@@ -46,6 +47,7 @@ resource "dependencytrack_project" "test" {
 					resource.TestCheckResourceAttr("dependencytrack_project.test", "description", ""),
 					resource.TestCheckResourceAttr("dependencytrack_project.test", "version", ""),
 					resource.TestCheckNoResourceAttr("dependencytrack_project.test", "parent"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "classifier", "APPLICATION"),
 				),
 			},
 		},
@@ -92,6 +94,85 @@ resource "dependencytrack_project" "child" {
 				Check: resource.TestCheckResourceAttrPair(
 					"dependencytrack_project.parent", "id",
 					"dependencytrack_project.child", "parent",
+				),
+			},
+		},
+	})
+}
+
+func TestAccProjectIdentity(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: providerConfig + `
+resource "dependencytrack_project" "test" {
+	name = "Test Project Identity Project"
+	group = "TestGroup"
+	purl = "pkg:npm/namespace/name@v1.0?k=v#subpath"
+	cpe = "cpe:2.3:a:ntp:ntp:4.2.8:p3:*:*:*:*:*:*"
+	swid = "Test_SWID"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "group", "TestGroup"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "purl", "pkg:npm/namespace/name@v1.0?k=v#subpath"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "cpe", "cpe:2.3:a:ntp:ntp:4.2.8:p3:*:*:*:*:*:*"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "swid", "Test_SWID"),
+				),
+			},
+			// ImportState
+			{
+				ResourceName:      "dependencytrack_project.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read
+			{
+				Config: providerConfig + `
+resource "dependencytrack_project" "test" {
+	name = "Test Project Identity Project"
+	group = "TestGroup With Change"
+	purl = "pkg:npm/namespace/name@v1.0?k=v#subpath"
+	cpe = "cpe:2.3:a:ntp:ntp:4.2.8:p3:*:*:*:*:*:*"
+	swid = "Test_SWID"
+}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "group", "TestGroup With Change"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "purl", "pkg:npm/namespace/name@v1.0?k=v#subpath"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "cpe", "cpe:2.3:a:ntp:ntp:4.2.8:p3:*:*:*:*:*:*"),
+					resource.TestCheckResourceAttr("dependencytrack_project.test", "swid", "Test_SWID"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccProjectVersion(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: providerConfig + `
+resource "dependencytrack_project" "test" {
+	name = "Test Project With Version"
+	version = "Test_Version"
+}
+data "dependencytrack_project" "data" {
+	name = dependencytrack_project.test.name
+	version = dependencytrack_project.test.version
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("dependencytrack_project.test", "id"),
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_project.test", "id",
+						"data.dependencytrack_project.data", "id",
+					),
+					resource.TestCheckResourceAttr("data.dependencytrack_project.data", "name", "Test Project With Version"),
+					resource.TestCheckResourceAttr("data.dependencytrack_project.data", "version", "Test_Version"),
 				),
 			},
 		},
