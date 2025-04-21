c1845f7055afa1972717ffe7b89f297345f88068
SolarFactories <SolarFactories@users.noreply.github.com>
2025-04-21 22:27:11 +0100

    Added read-only fields in 4.13 to dependencytrack_team_apikey. Updated logic to use publicIdOrKey, in places, to ensure continuity across versions.

--- examples/resources/dependencytrack_team_apikey/import.sh
+++ examples/resources/dependencytrack_team_apikey/import.sh
@@ -1,1 +1,4 @@
+# API <4.13 / Legacy API Keys
 terraform import dependencytrack_team_apikey.example 51e49752-6039-404b-bd4d-02e5d624a934/odt_6VTCwNpiihw1WmHhuEn48u39AGMkqfez
+# API >=4.13 & Non-Legacy
+terraform import dependencytrack_teap_apikey.example 51e49752-6039-404b-bd4d-02e5d624a934/xRJid7BI
