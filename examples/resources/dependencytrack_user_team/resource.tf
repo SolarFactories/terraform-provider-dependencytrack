5b6cdda5d605c58e61d50f89d28ade80759c0918
SolarFactories <SolarFactories@users.noreply.github.com>
2026-01-05 12:47:51 +0000

    Add example for dependencytrack_user_permission, and dependencytrack_user_team.

---
+++ examples/resources/dependencytrack_user_team/resource.tf
@@ -0,0 +1,14 @@
+resource "dependencytrack_team" "example" {
+  name = "Example"
+}
+resource "dependencytrack_user" "example" {
+  username = "Example"
+  fullname = "Example User"
+  email    = "Example_User@example.com"
+  password = "Initial_User_Password"
+}
+
+resource "dependencytrack_user_team" "example" {
+  username = dependencytrack_user.example.username
+  team     = dependencytrack_team.example.id
+}
