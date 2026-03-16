679fc9b09b273a76b96935ca1a7e4b8722db8917
SolarFactories <SolarFactories@users.noreply.github.com>
2026-03-16 21:00:32 +0000

    Added regression test for assiging OIDC users to a team.

--- internal/provider/user_team_resource_test.go
+++ internal/provider/user_team_resource_test.go
@@ -69,3 +69,54 @@ resource "dependencytrack_user_team" "test" {
 		},
 	})
 }
+
+func TestAccUserTeamResourceSSORegression177(t *testing.T) {
+	// Regression test for https://github.com/SolarFactories/terraform-provider-dependencytrack/issues/177
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing.
+			{
+				Config: providerConfig + `
+resource "dependencytrack_team" "test" {
+	name = "Test_Team_Regression177"
+}
+
+resource "dependencytrack_oidc_user" "test" {
+	username = "Test_User_Regression177_OIDC"
+}
+resource "dependencytrack_ldap_user" "test" {
+	username = "Test_User_Regression177_LDAP"
+}
+
+resource "dependencytrack_user_team" "oidc" {
+	username = dependencytrack_oidc_user.test.username
+	team = dependencytrack_team.test.id
+}
+resource "dependencytrack_user_team" "ldap" {
+	username = dependencytrack_ldap_user.test.username
+	team = dependencytrack_team.test.id
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_user_team.oidc", "username",
+						"dependencytrack_oidc_user.test", "username",
+					),
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_user_team.oidc", "team",
+						"dependencytrack_team.test", "id",
+					),
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_user_team.ldap", "username",
+						"dependencytrack_ldap_user.test", "username",
+					),
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_user_team.ldap", "team",
+						"dependencytrack_team.test", "id",
+					),
+				),
+			},
+		},
+	})
+}
