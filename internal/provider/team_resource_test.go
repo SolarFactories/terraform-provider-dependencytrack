3670ae9e4e23bd932c173c1a10bdead880fab2ee
SolarFactories <SolarFactories@users.noreply.github.com>
2026-09-06 15:30:59 +0100

    Add regression test for issue 236.

--- internal/provider/team_resource_test.go
+++ internal/provider/team_resource_test.go
@@ -43,3 +43,49 @@ resource "dependencytrack_team" "test" {
 		},
 	})
 }
+
+func TestAccTeamResourceRegression236(t *testing.T) {
+	// Regression test for https://github.com/SolarFactories/terraform-provider-dependencytrack/issues/236
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create initial team.
+			{
+				Config: providerConfig + `
+resource "dependencytrack_team" "test" {
+	name = "Test_Team_236"
+}
+`,
+			},
+			// Duplicate reference to team.
+			{
+				Config: providerConfig + `
+resource "dependencytrack_team" "test" {
+	name = "Test_Team_236"
+}
+resource "dependencytrack_team" "test2" {
+	name = "Test_Team_236"
+}
+import {
+	to = dependencytrack_team.test2
+	id = dependencytrack_team.test.id
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"dependencytrack_team.test", "id",
+						"dependencytrack_team.test2", "id",
+					),
+				),
+			},
+			// Delete initial team, and read copied reference.
+			{
+				Config: providerConfig + `
+resource "dependencytrack_team" "test2" {
+	name = "Test_Team_236"
+}
+`,
+			},
+		},
+	})
+}
