bb91887d967d10f13aec20455b8c36f00f833b5a
SolarFactories <SolarFactories@users.noreply.github.com>
2026-09-11 00:21:50 +0100

    One more Semver check in test.

--- internal/provider/tag_policies_resource_test.go
+++ internal/provider/tag_policies_resource_test.go
@@ -103,6 +103,9 @@ resource "dependencytrack_tag_policies" "test" {
 }
 
 func TestAccTagPoliciesResourcePoliciesUnordered(t *testing.T) {
+	if apiSemver.Major < 4 || (apiSemver.Major == 4 && apiSemver.Minor < 12) {
+		t.SkipNow()
+	}
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
